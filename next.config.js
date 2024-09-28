a48d2cb29653c5fe9680e5924715f6c9162fd2ad
sam-whitley <sam.whitley@hotmail.com>
2024-09-28 18:45:52 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Add this line for static HTML export
-  images: {
-    path: '/_next/image',
-  },
+  images: { unoptimized: true }
 };
 
 module.exports = withContentlayer(nextConfig);
